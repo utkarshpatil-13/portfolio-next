2e6959ebfc322a4eb57d37ed301345724e6c0da6
Utkarsh <utkarshpatil.it@gmail.com>
2024-05-23 16:03:45 +0530

    change app desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Utkarsh's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "My Personal Portfolio",
 };
 
 export default function RootLayout({
